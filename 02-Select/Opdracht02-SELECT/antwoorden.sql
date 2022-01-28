1cba75b9de87dfb43ac01632096baa5527935257
noah elmont <2047541@talnet.nl>
2022-01-28 11:47:30 +0100

    sql

--- 02-Select/Opdracht02-SELECT/antwoorden.sql
+++ 02-Select/Opdracht02-SELECT/antwoorden.sql
@@ -1,12 +1,12 @@
 -- Opdracht 1 - voorbeeld
 SELECT * FROM jaar2016
 -- Opdracht 2 
-
+SELECT * FROM jaar2015 WHERE rank = 1;
 -- Opdracht 3
-
+SELECT * FROM `jaar2016` WHERE score;
 -- Opdracht 4
-
+Select country, sore From jaar2016;
 -- Opdracht 5
-
+Select region, socre from jaar2015;
 -- Opdracht 6
-
+Select score From jaar2016;
