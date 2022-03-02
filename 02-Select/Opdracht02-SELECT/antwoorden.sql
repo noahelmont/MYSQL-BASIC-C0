05bd88009af51ad57c2579ef01ffdbd24273788a
noah elmont <2047541@talnet.nl>
2022-03-02 09:48:29 +0100

    mysql

--- 02-Select/Opdracht02-SELECT/antwoorden.sql
+++ 02-Select/Opdracht02-SELECT/antwoorden.sql
@@ -1,12 +1,12 @@
 -- Opdracht 1 - voorbeeld
 SELECT * FROM jaar2016
 -- Opdracht 2 
-SELECT * FROM jaar2015 WHERE rank = 1;
+SELECT * FROM jaar2015
 -- Opdracht 3
-SELECT * FROM jaar2016 score;
--- Opdracht 4
 Select country, score From jaar2016;
+-- Opdracht 4
+Select region From jaar2015;
 -- Opdracht 5
 Select region, score from jaar2015;
 -- Opdracht 6
-Select score From jaar2016;
+Select score, rank From jaar2016;
